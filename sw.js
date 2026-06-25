72b4f6ca006f03bc49c0b5e362eb3c253e1dddd6
a01030108199-stack <a01030108199@gmail.com>
2026-06-26 00:07:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'masoun-v1';
+const CACHE_NAME = 'masoun-v2';
 const ASSETS = [
   'index.html',
   'styles.css',
@@ -12,6 +12,20 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
